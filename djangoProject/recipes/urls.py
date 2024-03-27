f7583a7948c227d0191dc4b429e0ed59a387eb87
vikksata <vnaviakov@gmail.com>
2024-03-28 01:13:41 +0200

    added templates

---
+++ djangoProject/recipes/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.recipe_list, name='recipe_list'),
+    path('recipe/<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
+    path('recipe/add/', views.add_recipe, name='add_recipe'),
+    path('recipe/<int:recipe_id>/edit/', views.edit_recipe, name='edit_recipe'),
+    path('recipe/<int:recipe_id>/delete/', views.delete_recipe, name='delete_recipe'),
+]

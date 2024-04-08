d3f2302e5da98d82d0a1c3e20f23c0ed0c3f91d3
vikksata <vnaviakov@gmail.com>
2024-04-09 02:18:08 +0300

    added favorites

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-from .views import MyRecipesView, ProfileView, RecipeListView, RecipeDetailView
+from .views import MyRecipesView, ProfileView, RecipeListView, RecipeDetailView, add_favourite, user_favourites, \
+    FavoritesRecipeDetailView
 
 urlpatterns = [
     path('accounts/profile/', RecipeListView.as_view(), name='recipe_list'),
@@ -10,4 +11,7 @@ urlpatterns = [
     path('recipe/<int:recipe_id>/delete/', views.delete_recipe, name='delete_recipe'),
     path('my_recipes/', MyRecipesView.as_view(), name='my_recipes'),
     path('my_profile/', ProfileView.as_view(), name='my_profile'),
+    path('recipe/<int:recipe_id>/add_favourite/', add_favourite, name='add_favourite'),
+    path('user/<int:user_id>/favourites/', user_favourites, name='user_favourites'),
+    path('favorites/recipe/<int:pk>/', FavoritesRecipeDetailView.as_view(), name='favorite_recipe_details'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import logout, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import Group
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, TemplateView, DetailView
 
-from .models import Recipe, CustomUser
+from .models import Recipe, CustomUser, Favorite
 from .forms import RecipeForm, EditRecipeForm
 from .forms import RegistrationForm
 
@@ -108,3 +109,28 @@ class MyRecipesView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Recipe.objects.filter(author=self.request.user)
+
+
+@login_required
+def add_favourite(request, recipe_id):
+    recipe = Recipe.objects.get(id=recipe_id)
+    # Check if the favourite already exists
+    existing_favourite = Favorite.objects.filter(user=request.user, recipe=recipe)
+    if existing_favourite.exists():
+        return HttpResponse("This recipe is already in your favourites.")
+
+    # If the favourite does not exist, create a new one
+    favourite = Favorite(user=request.user, recipe=recipe)
+    favourite.save()
+    return redirect('home')
+
+
+@login_required
+def user_favourites(request, user_id):
+    favorites = Favorite.objects.filter(user__id=user_id)
+    return render(request, 'recipes/user_favourites.html', {'favorites': favorites})
+
+
+class FavoritesRecipeDetailView(DetailView):
+    model = Recipe
+    template_name = 'recipes/favorite_recipe_details.html'
